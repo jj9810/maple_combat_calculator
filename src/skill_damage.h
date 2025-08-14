376414be6b6e69cdd4a9275249c58e943532afa3
jj9810 <cheesej98@gmail.com>
2025-08-14 19:23:28 +0900

    Initial Commit

---
+++ src/skill_damage.h
@@ -0,0 +1,18 @@
+//
+// Created by ubuntu on 25. 8. 10..
+//
+
+#ifndef MCC_SKILL_DAMAGE_H
+#define MCC_SKILL_DAMAGE_H
+
+constexpr long long DEFAULT_MAX_DAMAGE = 700000000000;
+
+#define MAX_DAMAGE_CAP true
+
+constexpr double CRIT_RATIO_MIN = 1.2;
+constexpr double CRIT_RATIO_MEAN = 1.35;
+constexpr double CRIT_RATIO_MAX = 1.5;
+
+constexpr double ELEM_RES = 0.5;
+
+#endif //MCC_SKILL_DAMAGE_H
